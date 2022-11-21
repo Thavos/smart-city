a0bcfbc0aba93d39ae7f7d7365120ca44a7ad1d4
Šimon Hrúz <simon.hruz252@gmail.com>
2022-11-21 19:27:26 +0100

    init fix

--- server/prisma/migrations/20221020090253_resident/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Resident" (
-    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "name" STRING NOT NULL,
-
-    CONSTRAINT "Resident_pkey" PRIMARY KEY ("id")
-);
--- server/src/resident/resident.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { ResidentService } from './resident.service';
-import { ResidentResolver } from './resident.resolver';
-import { PrismaService } from 'src/prisma/prisma.service';
-
-@Module({
-  providers: [ResidentResolver, ResidentService, PrismaService],
-})
-export class ResidentModule {}
