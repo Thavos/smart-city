d0e21a5aaa678bad60ea045e52db996cf082580a
Šimon Hrúz <simon.hruz252@gmail.com>
2022-11-24 23:49:19 +0100

    fix endpoints

--- client/src/pages/Profile/Profile.tsx
+++ client/src/pages/Profile/Profile.tsx
@@ -38,7 +38,7 @@ export const Profile: FC = () => {
   };
 
   useEffect(() => {
-    fetch("/graphql", {
+    fetch("/api/graphql", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
--- client/src/pages/Register/Register.tsx
+++ client/src/pages/Register/Register.tsx
@@ -16,7 +16,7 @@ export const Register: FC = () => {
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
-    await fetch("/graphql", {
+    await fetch("/api/graphql", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
